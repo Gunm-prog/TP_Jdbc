5202f8a191174b6a30f5e08f3116f70a333c7d77
Emilie Balsen <54776673+Gunm-prog@users.noreply.github.com>
2023-12-22 15:53:51 +0100

    first commit

---
+++ src/Entity/Client.java
@@ -0,0 +1,10 @@
+package Entity;
+
+import java.io.Serializable;
+
+public class Client implements Serializable {
+
+    public static final long serialVersionUID = 1L;
+
+
+}
---
+++ src/Entity/User.java
@@ -0,0 +1,17 @@
+package Entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private int employeeNb;
+    private String lastname;
+    private String firstname;
+    private String email;
+    private String login;
+    private String password;
+    private boolean isConnected;
+}

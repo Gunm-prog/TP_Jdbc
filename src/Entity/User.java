7e7abcd7ed6c83eb542eea107a7932aee17dae19
Emilie Balsen <54776673+Gunm-prog@users.noreply.github.com>
2024-01-02 11:17:30 +0100

    getter, setters and toString in User Entity

--- src/Entity/User.java
+++ src/Entity/User.java
@@ -14,4 +14,74 @@ public class User implements Serializable {
     private String login;
     private String password;
     //private boolean isConnected;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getEmployeeNb() {
+        return employeeNb;
+    }
+
+    public void setEmployeeNb(int employeeNb) {
+        this.employeeNb = employeeNb;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", employeeNb=" + employeeNb +
+                ", lastname='" + lastname + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", email='" + email + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
